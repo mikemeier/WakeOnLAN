e353b8666a3d784504aadec68a1691ab138c9158
Mike Meier <mike.meier@ibrows.ch>
2012-11-12 16:13:49 +0100

    Add composer functionality

--- Example/simple.php
+++ Example/simple.php
@@ -1,5 +1,4 @@
 <?php
 
-require_once __DIR__.'/../PHPGangsta/WakeOnLAN.php';
-
+require_once __DIR__.'/../vendor/autoload.php';
 \PHPGangsta\WakeOnLAN::wakeUp('40:61:86:38:3D:11', '192.168.1.255');
